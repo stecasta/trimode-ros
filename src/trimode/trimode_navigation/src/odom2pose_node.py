5e29b630453b5ab459b3db4d230b282cd9b1ad1c
stecasta <stefanocastagnetta@gmail.com>
2019-06-27 15:04:00 +0800

    minor

---
+++ src/trimode/trimode_navigation/src/odom2pose_node.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+ 
+import rospy
+import std_msgs
+import tf
+ 
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import PoseWithCovarianceStamped
+ 
+pose = PoseWithCovarianceStamped()
+ 
+def fnc_callback(odom):
+    #build posewithcovariancestamped msg
+    global pose
+    pose.header = odom.header
+    pose.pose = odom.pose
+   
+if __name__=='__main__':
+    rospy.init_node('odom2pose')
+   
+    sub=rospy.Subscriber('odom', Odometry, fnc_callback)
+    pub=rospy.Publisher('pose', PoseWithCovarianceStamped, queue_size=1)
+    rate=rospy.Rate(20)
+ 
+    while not rospy.is_shutdown():
+   
+        pub.publish(pose)
+        rate.sleep()
